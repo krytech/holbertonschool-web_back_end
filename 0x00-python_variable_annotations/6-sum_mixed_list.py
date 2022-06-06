44a364a1e070eede1482cf206d63e9f8d97c239d
Carson Stearn <cstearn@gmail.com>
2022-06-06 18:49:08 +0000

    t6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Complex types - mixed list"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns sum as a float"""
+    return sum(mxd_lst)
