9e2ffca43feead954e3a8141def845de532957bf
krytech <cstearn@gmail.com>
2022-06-06 19:26:06 +0000

    t7-9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns  function that multiples a float by a multiplier"""
+    def multi_func(number: float) -> float:
+        return number * multiplier
+    return multi_func
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Duck typing"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns element length"""
+    return [(i, len(i)) for i in lst]
