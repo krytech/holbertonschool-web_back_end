2e5f3e3303e9cde30209a7a32d060b73f7e486cb
krytech <cstearn@gmail.com>
2022-06-10 15:40:57 +0000

    pycodestyle

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,7 +7,7 @@ class BasicCache(BaseCaching):
     """ inherits from BaseCaching and is a caching system """
     def put(self, key, item):
         """ assign item for key in cache_data """
-        if  key is None or item is None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
 
