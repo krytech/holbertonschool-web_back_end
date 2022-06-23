4e830996d008a8b48b259a959709682729c99ca4
krytech <cstearn@gmail.com>
2022-06-23 16:44:32 +0000

    comments

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -66,13 +66,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 
 def main() -> None:
     """
-    Obtain a database connection using get_db
-    and retrieve all rows in the users table and display each row w/ format:
-    [HOLBERTON] user_data INFO 2019-11-19 18:37:59,596:
-    name=***; email=***; phone=***; ssn=***; password=***;
-    ip=60ed:c396:2ff:244:bbd0:9208:26f2:93ea; last_login=2019-11-14 06:14:24
-    user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36;
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
     """
     db = get_db()
     cursor = db.cursorsor()
