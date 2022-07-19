b57587022e44a214d6a9df4e053732e63af9a002
krytech <cstearn@gmail.com>
2022-07-19 16:08:53 +0000

    t2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ 0x0B. Redis basic
 """
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
 
@@ -15,7 +15,23 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Generates a random uuid key. """
+        """ Generates a random uuid key, stores in Redis. """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        """ Converts the data back to the desired format. """
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, bytes, int, float]:
+        """ Converts the data back to the desired format. """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Union[str, bytes, int, float]:
+        """ Converts the data back to the desired format. """
+        return self.get(key, int)
