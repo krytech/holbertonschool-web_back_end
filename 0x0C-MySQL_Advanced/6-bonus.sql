8fc2e9901081736c7d56cca592a89aa6f7f8b46d
krytech <cstearn@gmail.com>
2022-07-21 17:24:52 +0000

    typo

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -4,13 +4,13 @@
 DELIMITER |
 CREATE PROCEDURE AddBonus (
   IN user_id int,
-  INT project_name carchar(255),
+  INT project_name varchar(255),
   IN score float
 )
 BEGIN
   INSERT INTO projects (name)
   SELECT project_name
-  WHERE NOT ECXISTS (SELECT * FROM projects WHERE name = project_name);
+  WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
   INSERT INTO corrections (user_id, project_id, score)
   VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END;
