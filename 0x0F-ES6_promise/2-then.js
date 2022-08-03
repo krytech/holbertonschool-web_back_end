8304a1b9aac76b7040cf975a0eda44bb1e1a33b0
krytech <cstearn@gmail.com>
2022-08-03 18:05:00 +0000

    typo

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' }))
+  return promise.then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error()).finally(() => console.log('Got a response from the API'));
 }
