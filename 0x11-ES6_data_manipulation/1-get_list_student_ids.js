9fb79c02bde2f3ec3eae389c0b038276baac4f74
krytech <cstearn@gmail.com>
2022-08-09 16:30:07 +0000

    all js files

---
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  const map = students.map((mappedObj) => mappedObj.id);
+  return map;
+}
