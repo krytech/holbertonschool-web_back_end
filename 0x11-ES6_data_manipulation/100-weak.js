714b0de5c7ef5d27a3379f251f0dd66c99764d29
krytech <cstearn@gmail.com>
2022-08-09 16:56:41 +0000

    task 100 typo

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -6,5 +6,5 @@ export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) queries = weakMap.get(endpoint);
   weakMap.set(endpoint, queries + 1);
 
-  if (queries + 1 >= 5) throw Error('Endpoint load is too high');
+  if (queries + 1 >= 5) throw Error('Endpoint load is high');
 }
